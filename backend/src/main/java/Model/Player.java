006ade478d177941a0a3d5766956f2aa06c4da57
Lupsa Sergiu <sergiul2000@yahoo.com>
2023-04-04 09:33:09 +0300

    small modifications

--- backend/src/main/java/Model/Player.java
+++ backend/src/main/java/Model/Player.java
@@ -2,15 +2,21 @@ package Model;
 
 import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.Objects;
 
 @Entity
 @Table(name= "player")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Player {
-    public Player() {
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,71 +33,7 @@ public class Player {
     @Column()
     private Integer weight;
 
-    public Player(Integer id, String name, Integer age, String position, Integer height, Integer weight) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.position = position;
-        this.height = height;
-        this.weight = weight;
-    }
 
-    public Integer getId() {
-        return id;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    public Integer getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Integer weight) {
-        this.weight = weight;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", position='" + position + '\'' +
-                ", height=" + height +
-                ", weight=" + weight +
-                '}';
-    }
 
     @Override
     public boolean equals(Object o) {
